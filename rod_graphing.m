56d3f066b7b59bc1357f7b80bd747b8ed5569788
KeenanMcConkey <k.t.mcconkey@gmail.com>
2017-05-31 15:13:20 -0700

    MATLAB code

--- rod_graphing.m
+++ rod_graphing.m
@@ -3,14 +3,14 @@
 x = [0.0 6.0 12.0 18.0 24.0 30.0]; % Location of sensors
 
 % Read data files
-datafile = '/Users/Keenan/Documents/Processing/HeatTransport/HeatTransport_2017_5_29.csv';
-raw_data = csvread(datafile, 1, 0);
-volt = csvread(datafile, 1, 7, [1 7 (size(raw_data,1)-1) 12]);
-temp = csvread(datafile, 1, 13, [1 13 (size(raw_data,1)-1) 18]);
-time = linspace(0, size(volt,1), size(volt,1));
+datafile = '/Users/Keenan/Documents/Processing/HeatTransport/HeatTransport_2017_5_31.csv';
+raw_data = csvread(datafile, 1, 0); % Use csvread function to read our .csv data files
+volt = csvread(datafile, 1, 7, [1 7 (size(raw_data,1)-1) 12]); % Specify the columns that 
+temp = csvread(datafile, 1, 13, [1 13 (size(raw_data,1)-1) 18]); % contain volt/temp data
+time = linspace(0, size(volt,1), size(volt,1)); % Based on size of the other data
 
 close all;
-figure('Name', 'ENPH257 - Aluminium Rod Data Graph'); % Graphing data
+figure('Name', 'ENPH257 - Aluminium Rod Data Graph'); % Graph specifications
 title('Graph of Temperature At Varying Points on an Aluminium Rod');
 ylim([290.0 340.0]);
 xlabel('Distance x (cm)');
@@ -26,14 +26,17 @@ for i=1:size(temp,1)
     clearpoints(h);
     addpoints(h,x,temp(i,:));
     drawnow limitrate;
-    pause(0.00001);
+    pause(0.01);
 end
 
 figure('Name', 'ENPH257 - Temperature for Each Sensor Over Time');
 title('Graph of Temperature for Each Sensor Over Time');
+
 xlabel('Time t (s)');
 ylabel('Temperature T (K)');
+ylim([290.0 340.0]);
 hold on;
+
 for n=1:6
     plot(time, temp(:,n));
     hold on;
