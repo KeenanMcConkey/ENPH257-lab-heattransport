24037bbeb2c386e7078acd6709051ff211cbc40d
KeenanMcConkey <k.t.mcconkey@gmail.com>
2017-05-31 15:12:02 -0700

    New code

--- HeatTransport.ino
+++ HeatTransport.ino
@@ -2,7 +2,7 @@
 // ENPH 257 - Heat Transport
 
 int numPins = 6;
-int sensorPins[] = {A5, A4, A3, A2, A1, A0}; // Initialize pins
+int sensorPins[] = {A0, A1, A2, A3, A4, A5}; // Initialize pins
 
 void setup() {
   Serial.begin(9600); // Start serial @ 9600 bauds 
@@ -14,9 +14,6 @@ void loop() {
   float tempReadings[numPins];
   for (int i = 0; i < numPins; i++) { // Read from all pins
     voltageReadings[i] = analogRead(sensorPins[i]) * 3.3/1023.0; // Convert 10 bit to V
-    if (i == 4) {
-      voltageReadings[i] + 0.05;
-    }
     tempReadings[i] = (voltageReadings[i]) * 100.0; // Convert 10mV/˚C
   } // Print data to serial for reading
   for (int i = 0; i < numPins; i++) {
