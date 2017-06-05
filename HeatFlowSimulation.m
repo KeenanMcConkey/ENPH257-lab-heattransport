7df854cb6a8cb3d987e74e0fe13ae654f3388f97
danielzlkor <daniel.zl.kor@gmail.com>
2017-06-05 15:22:35 -0500

    Add files via upload

--- HeatFlowSimulation.m
+++ HeatFlowSimulation.m
@@ -1,18 +1,31 @@
 function HeatFlowSimulation 
 
+  
+%Experiemetal data. The data helps determine the dt and time we want to use
+%in our simulation
+ 
+%File to read.
+datafile = 'C:\Users\Daniel Kor\Desktop\HeatTransport_2017_6_2.csv';
+[TempExpData,ExpDt,ExpTotalTime]= ReadExpAndPlot(datafile);
+
+
+%Go to Simulation  
+
 %Initiailize Constants:
 length = 0.3; %[m]
 numSlice = 50;
-Time = 100000; 
-
-dt = 0.1; %[s]
+dt = 0.1;
 dx = length/numSlice; %[m]
 
-numTime = Time/dt;
+%Initialize time to match experimental data.
+Time = ExpTotalTime; 
+coolingInterval = 65;
 
+numTime = Time/dt;
 sensors = 6; 
 
 sigma = 5.67367E-8; %[W*m^-2*K^-4] Stefan-Boltzmann constant 
+c = 900; %Specific heat capacity [J/(kgK)]
 
 CtoK = 273;
 
@@ -25,33 +38,51 @@ surface_area= 2*pi*radius*dx;
 rho = 2700; %density of aluminum.
 %mass of aluminum.
 
-%Initialize Test values (TBD).
+%Variable Parameters.
+
+%Note:
+%Split the parameter into 3 different regions: First slice, middle slices,
+%and last slice.
+
+%For each slice, we have different conductivity(k), convection current (kc)
+%and emissivity (eps).
 
 voltage = 15; %check 
-resistance = 20; %check 
+resistance = 15; %check 
 powerIn = voltage^2 / resistance;
-powerEff = 0.70; %account for other things, like whether the power is actually going through the rod.
-eps = 1; %account for the fact of more glue.
-sigma = 5.67367E-8; %Stefan-Boltzmann constant.
-kc = 12.7; %Convection constant [W/m^2/K].
-k = 200; %Conductivity [W/mK]
-c = 900; %Specific heat capacity [J/(kgK)]
+powerEff = 1; %account for other things, like whether the power is actually going through the rod.
 
+%First slice parameters:
+epsFirst = 1.0; %Emissivity. High because of the power resistor's black case.
+kcFirst = 20.0; %Convection constant [W/m^2/K].
+kFirst = 200; %Conductvity.
+
+%Mid slices parameters:
+epsMid = 0.5; %Emissivity. High because of the power resistor's black case.
+kcMid = 20.0; %Convection constant [W/m^2/K].
+kMid = 200; %Conductvity.
+
+%Last slice parameters:
+epsLast = 0.5; %Emissivity. High because of the power resistor's black case.
+kcLast = 20.0; %Convection constant [W/m^2/K].
+kLast = 200; %Conductvity.
 
 %Concise Calculations
 
 C3 = c*rho*dx^2;
 C = c*rho*pi*radius^2 * dx;
-alpha = k/(c*rho); 
 
 
 %Ambient Temperature 
-Tamb = CtoK + 20;
+Tamb = TempExpData(1:1);
 
 
-%Create the temperature and length array:
+%Create the temperature, time and sensor length array:
+t = linspace(0,Time,numTime); 
+%ExpT = linspace(0,
 Temp = ones(numTime,numSlice); %The entries are the temperatures at a given position and time. 
 x = linspace(0,length,numSlice); 
+sensorLength = [0.0,6.0,12.0,18.0,24.0,30.0];
 
 %Initial Conditions:
 % Theat = CtoK + 50; 
@@ -60,46 +91,97 @@ Temp(1,1:numSlice) = Tamb;
 
 %Loop in time 
 
+HeatingFlag = 1;
 
 for m =2:numTime
-    
-    %Start with the first slice. 
-    
-    Pin = powerIn*powerEff;
-    
-    Ploss = (surface_area + cross_area )*(  kc * (Temp(m-1,1) - Tamb ) + sigma * eps * ( Temp(m-1,1).^4 - Tamb.^4 ) );
-    
-    Pactual = Pin - Ploss;
-    
-    Temp(m ,1) = Temp(m-1,1) + dt * ( (Pactual/C) - k * ( Temp(m-1,1) - Temp(m-1,2) ) /C3 );
-    
-    %Mid Slices
-    
-    Temp(m,2:numSlice-1) = Temp(m-1,2:numSlice-1) + dt* k * ( Temp(m-1,1:numSlice-2) - 2 * Temp(m-1,2:numSlice-1) + Temp(m-1, 3:numSlice) )/C3;
-    
-    Ploss =  (surface_area) * (  kc * (Temp(m-1,2:numSlice-1) - Tamb ) + sigma * eps * ( Temp(m-1,2:numSlice-1).^4 - Tamb.^4 ) );
-    
-    Temp(m,2:numSlice-1) = Temp(m,2:numSlice-1) - dt * ( Ploss / C ); 
-    
-    %End slice.
-    
-    Temp(m,numSlice) = Temp (m-1,numSlice) +  (dt * k * ( Temp(m-1,numSlice-1) - Temp(m-1,numSlice) ) )/C3 ;
-    
-    Ploss = (surface_area + cross_area )*( kc * (Temp(m-1,numSlice) - Tamb ) + sigma * eps * ( Temp(m-1,numSlice).^4 - Tamb.^4 ) );
-    
-    Temp(m,numSlice) = Temp (m,numSlice) -  dt * ( Ploss / C ); 
    
-%      plot(x,Temp(m,:));
-%  
-%      drawnow;
-%     
+    if  mod(t(m),coolingInterval) < 0.1
+        if HeatingFlag == 0
+            HeatingFlag = 1;
+        else
+            HeatingFlag = 0;
+        end
+    end
+    
+    if HeatingFlag == 0
+        Pin = 0;
+        
+        Ploss = (surface_area + cross_area )*(  kcFirst * (Temp(m-1,1) - Tamb ) + sigma * epsFirst * ( Temp(m-1,1).^4 - Tamb.^4 ) );
+
+        Pactual = Pin - Ploss;
+
+        Temp(m ,1) = Temp(m-1,1) + dt * ( (Pactual/C) - kFirst * ( Temp(m-1,1) - Temp(m-1,2) ) /C3 );
+
+        %Mid Slices
+
+        Temp(m,2:numSlice-1) = Temp(m-1,2:numSlice-1) + dt* kMid * ( Temp(m-1,1:numSlice-2) - 2 * Temp(m-1,2:numSlice-1) + Temp(m-1, 3:numSlice) )/C3;
+
+        Ploss =  (surface_area) * (  kcMid * (Temp(m-1,2:numSlice-1) - Tamb ) + sigma * epsMid * ( Temp(m-1,2:numSlice-1).^4 - Tamb.^4 ) );
+
+        Temp(m,2:numSlice-1) = Temp(m,2:numSlice-1) - dt * ( Ploss / C ); 
+
+        %End slice.
+
+        Temp(m,numSlice) = Temp (m-1,numSlice) +  (dt * kLast * ( Temp(m-1,numSlice-1) - Temp(m-1,numSlice) ) )/C3 ;
+
+        Ploss = (surface_area + cross_area )*( kcLast * (Temp(m-1,numSlice) - Tamb ) + sigma * epsLast * ( Temp(m-1,numSlice).^4 - Tamb.^4 ) );
+
+        Temp(m,numSlice) = Temp (m,numSlice) -  dt * ( Ploss / C ); 
+    end 
+    
+    if HeatingFlag == 1
+        Pin = powerIn*powerEff;
+    
+        Ploss = (surface_area + cross_area )*(  kcFirst * (Temp(m-1,1) - Tamb ) + sigma * epsFirst * ( Temp(m-1,1).^4 - Tamb.^4 ) );
+
+        Pactual = Pin - Ploss;
+
+        Temp(m ,1) = Temp(m-1,1) + dt * ( (Pactual/C) - kFirst * ( Temp(m-1,1) - Temp(m-1,2) ) /C3 );
+
+        %Mid Slices
+
+        Temp(m,2:numSlice-1) = Temp(m-1,2:numSlice-1) + dt* kMid * ( Temp(m-1,1:numSlice-2) - 2 * Temp(m-1,2:numSlice-1) + Temp(m-1, 3:numSlice) )/C3;
+
+        Ploss =  (surface_area) * (  kcMid * (Temp(m-1,2:numSlice-1) - Tamb ) + sigma * epsMid * ( Temp(m-1,2:numSlice-1).^4 - Tamb.^4 ) );
+
+        Temp(m,2:numSlice-1) = Temp(m,2:numSlice-1) - dt * ( Ploss / C ); 
+
+        %End slice.
+
+        Temp(m,numSlice) = Temp (m-1,numSlice) +  (dt * kLast * ( Temp(m-1,numSlice-1) - Temp(m-1,numSlice) ) )/C3 ;
+
+        Ploss = (surface_area + cross_area )*( kcLast * (Temp(m-1,numSlice) - Tamb ) + sigma * epsLast * ( Temp(m-1,numSlice).^4 - Tamb.^4 ) );
+
+        Temp(m,numSlice) = Temp (m,numSlice) -  dt * ( Ploss / C ); 
+    end 
+
     
 end 
-  plot(x,Temp(numTime,:));
-  ylabel('Temperature (K)');
-  xlabel ('Length (m)');
+ 
+  figure()
+  Label = ones(1,6);
+  for ii = 1:6
+    actualLength = sensorLength(ii);
+    Label(ii) = ii;
+    for jj  = 1:numSlice
+        if actualLength == x(jj)
+            ii=jj;
+        end 
+    end
+     plot(t,Temp(:,ii)); 
+     ylabel('Temperature (K)');
+     xlabel ('Time(s)');  
+     ylim([290.0 350.0]);
+     title('Temperature measurements at Sensors over Time')
+     hold on;
+     
+     plot(t,TempExpData(:,ii),'.');
+    
+     hold on;
+    
+  end 
   
-  plot(x, 
+
 end 
 
 
